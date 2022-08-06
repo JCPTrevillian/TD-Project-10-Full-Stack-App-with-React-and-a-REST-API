dae4dbe0a1a4c4f200e451c681ed492391f61da0
Jennifer Trevillian <95890754+JCPTrevillian@users.noreply.github.com>
2022-08-06 01:19:56 -0700

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+import Header from './components/Header';
+import NotFound from './components/NotFound';
+import Forbidden from './components/Forbidden';
+import UnhandledError from './components/UnhandledError';
+import Courses from './components/Courses';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
+import CourseDetail from './components/CourseDetail';
+import UserSignIn from './components/UserSignIn';
+import UserSignUp from './components/UserSignUp';
+import UserSignOut from './components/UserSignOut';
+import PrivateRoute from './PrivateRoute';
+
+import withContext from './Context';
+
+const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignInWithContext = withContext(UserSignIn);
+const UserSignOutWithContext = withContext(UserSignOut);
+const HeaderWithContext = withContext(Header);
+const CreateCourseWithContext = withContext(CreateCourse);
+const UpdateCourseWithContext = withContext(UpdateCourse);
+const CourseDetailWithContext = withContext(CourseDetail);
+
+
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <HeaderWithContext />
+        <Switch>
+          <Route exact path="/" render={() => <Courses />} />
+          <PrivateRoute exact path="/courses/create" component={CreateCourseWithContext} />
+          <PrivateRoute exact path="/courses/:id/update" component={UpdateCourseWithContext} />
+          <Route exact path="/courses/:id" component={CourseDetailWithContext} />
+          <Route path="/signup" component={UserSignUpWithContext} />
+          <Route path="/signin" component={UserSignInWithContext} />
+          <Route path="/signout" component={UserSignOutWithContext} />
+          <Route path="/notfound" component={NotFound} />
+          <Route path="/forbidden" component={Forbidden}/>
+          <Route path="/error" component={UnhandledError} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>    
   );
+
 }
 
 export default App;
